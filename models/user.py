cb83099e44afce5f22016629969cb78682ae69ce
vkalcheva <violetakalcheva@abv.bg>
2021-12-19 15:57:31 +0200

    .

---
+++ models/user.py
@@ -0,0 +1,32 @@
+from db import db
+from models.enums import RoleType
+
+
+class BaseUserModel(db.Model):
+    __abstract__ = True
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(20), nullable=False)
+    last_name = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    phone = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+
+
+class ComplainerModel(BaseUserModel):
+    __tablename__ = "complainers"
+
+    iban = db.Column(db.String(22))
+    role = db.Column(db.Enum(RoleType), default=RoleType.complainer, nullable=False)
+
+
+class ApproverModel(BaseUserModel):
+    __tablename__ = "approvers"
+
+    certificate = db.Column(db.String(255), nullable=False)
+    role = db.Column(db.Enum(RoleType), default=RoleType.approver, nullable=False)
+
+
+class AdministratorModel(BaseUserModel):
+    __tablename__ = "administrators"
+    role = db.Column(db.Enum(RoleType), default=RoleType.admin, nullable=False)
---
+++ resources/complaint.py
@@ -0,0 +1,60 @@
+from flask import request
+from flask_restful import Resource
+
+from managers.auth import auth
+from managers.complaint import ComplaintManager
+from models.enums import RoleType
+from schemas.request.complaint import ComplaintCreateRequestSchema
+from schemas.response.complaint import ComplaintCreateResponseSchema
+from util.decorators import validate_schema, permission_required
+
+
+class ListCreateComplaint(Resource):
+    @auth.login_required
+    def get(self):
+        complaints = ComplaintManager.get_all()
+        schema = ComplaintCreateResponseSchema()
+        return schema.dump(complaints, many=True)
+
+    @auth.login_required
+    @permission_required(RoleType.complainer)
+    @validate_schema(ComplaintCreateRequestSchema)
+    def post(self):
+        current_user = auth.current_user()
+        complaint = ComplaintManager.create(request.get_json(), current_user.id)
+        schema = ComplaintCreateResponseSchema()
+        return schema.dump(complaint)
+
+
+class ComplaintDetail(Resource):
+    @auth.login_required
+    @permission_required(RoleType.complainer)
+    @validate_schema(ComplaintCreateRequestSchema)
+    def put(self, id_):
+        updated_complaint = ComplaintManager.update(request.get_json(), id_)
+        schema = ComplaintCreateResponseSchema()
+        return schema.dump(updated_complaint)
+
+    @auth.login_required
+    # @permission_required(RoleType.admin)
+    def delete(self, id_):
+        ComplaintManager.delete(id_)
+        return {"message": "Success"}, 204
+
+
+class ApproveComplaint(Resource):
+    @auth.login_required
+    # @permission_required(RoleType.approver)
+    def get(self, id_):
+        complaint = ComplaintManager.approve(id_)
+        schema = ComplaintCreateResponseSchema()
+        return schema.dump(complaint)
+
+
+class RejectComplaint(Resource):
+    @auth.login_required
+    # @permission_required(RoleType.approver)
+    def get(self, id_):
+        complaint = ComplaintManager.reject(id_)
+        schema = ComplaintCreateResponseSchema()
+        return schema.dump(complaint)
